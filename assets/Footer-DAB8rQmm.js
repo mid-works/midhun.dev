e8fb23802df485d65808697248251d3cc428e1e1
mid-works <midstask@gmail.com>
2025-10-31 21:36:42 +0530

    Updates

--- assets/Footer-DAB8rQmm.js
+++ assets/Footer-DAB8rQmm.js
@@ -1,4 +1,4 @@
-import{b as ce,g as we,d as ir,j as Q}from"./index-CLplGyGN.js";import{m as ke}from"./proxy-C-FjDMob.js";var sr={exports:{}},N={};/**
+import{b as ce,g as we,d as ir,j as Q}from"./index-CJ5_lm-2.js";import{m as ke}from"./proxy-D4fzOYS9.js";var sr={exports:{}},N={};/**
  * @license React
  * react.production.js
  *
