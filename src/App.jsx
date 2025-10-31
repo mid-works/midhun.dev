69999da96b044020305e5f3881a53ba70e4c24ed
mid-works <midstask@gmail.com>
2025-10-31 19:23:00 +0530

    A section

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,49 @@
-import { BrowserRouter } from "react-router-dom"
-import {About , Contact, Hero, Navbar, Tech, Works , Exp, StarsCanvas, Footer} from "./components"
+import { BrowserRouter } from "react-router-dom";
+import { Suspense, lazy } from "react";
+
+// Lazy load all major sections
+const Navbar = lazy(() => import("./components/Navbar"));
+const Hero = lazy(() => import("./components/Hero"));
+const About = lazy(() => import("./components/About"));
+const Exp = lazy(() => import("./components/Exp"));
+const Tech = lazy(() => import("./components/Tech"));
+const Works = lazy(() => import("./components/Works"));
+const Contact = lazy(() => import("./components/Contact"));
+const StarsCanvas = lazy(() => import("./components/canvas/Stars"));
+const Footer = lazy(() => import("./components/Footer"));
+
+// Optional loader component
+const Loader = () => (
+  <div className="flex items-center justify-center h-screen text-white text-lg">
+    Loading...
+  </div>
+);
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="relative z-0 bg-primary">
-        <div>
-          <Navbar />
-          <Hero />
-        </div>
-        <About />
-        <Exp />
-        <Tech />
-        <Works />
-        <div className="relative z-0">
-          <Contact />
-          <StarsCanvas />
-        </div>
-        <Footer />
-      </div>
-    </BrowserRouter>
-  ) 
-}
+      <Suspense fallback={<Loader />}>
+        <div className="relative z-0 bg-primary">
+          <div>
+            <Navbar />
+            <Hero />
+          </div>
 
-export default App
+          <About />
+          <Exp />
+          <Tech />
+          <Works />
+
+          <div className="relative z-0">
+            <Contact />
+            <StarsCanvas />
+          </div>
+
+          <Footer />
+        </div>
+      </Suspense>
+    </BrowserRouter>
+  );
+};
+
+export default App;
