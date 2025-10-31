7c12ca979baedd2f57f415a45f7b71a3d9f8b5bc
mid-works <midstask@gmail.com>
2025-10-31 19:28:38 +0530

    Updates

---
+++ assets/Styles-sejEAH3O.js
@@ -0,0 +1,1 @@
+const t={paddingX:"sm:px-16 px-6",padding:"sm:px-16 px-6 sm:py-16 py-10",sectionHeadText:"text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",sectionSubText:"sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider"};export{t as s};
